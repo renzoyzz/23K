b4270d9045fdde6412d1583bbccd3845f3be961f
renzoyzz <renzoyzevallos@gmail.com>
2017-12-11 23:52:01 -0800

    Add stats

---
+++ 23Knots/GameObjects/Properties/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23Knots.GameObjects.Properties
+{
+    public class Stats
+    {
+        public float Acceleration { get; set; }
+        public float MaxSpeed { get; set; }
+
+        public Stats(float acceleration = 0f, float maxSpeed = 0f)
+        {
+            Acceleration = acceleration;
+            MaxSpeed = maxSpeed;
+        }
+
+    }
+}
